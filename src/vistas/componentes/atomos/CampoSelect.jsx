6ef9a2c2ad014ffbca369ebf52a9ec18b106d986
NicoH00d <a01705857@tec.mx>
2025-04-16 13:06:20 -0600

    FEAT: Agregar componente Campo Select

---
+++ src/vistas/componentes/atomos/CampoSelect.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FormControl, InputLabel, Select, MenuItem, FormHelperText } from '@mui/material';
+
+/**
+ * Select personalizado utilizando Material UI.
+ *
+ */
+const CampoSelect = ({
+  label,
+  name,
+  value,
+  onChange,
+  options = [],
+  fullWidth = false,
+  required = false,
+  error = false,
+  helperText = '',
+  disabled = false,
+  size = 'small',
+  ...props
+}) => {
+  return (
+    <FormControl
+      fullWidth={fullWidth}
+      required={required}
+      error={error}
+      size={size}
+      disabled={disabled}
+    >
+      <InputLabel id={`${name}-label`}>{label}</InputLabel>
+      <Select
+        labelId={`${name}-label`}
+        id={name}
+        name={name}
+        value={value}
+        label={label}
+        onChange={onChange}
+        {...props}
+      >
+        {options.map((option) => (
+          <MenuItem key={option.value} value={option.value}>
+            {option.label}
+          </MenuItem>
+        ))}
+      </Select>
+      {helperText && <FormHelperText>{helperText}</FormHelperText>}
+    </FormControl>
+  );
+};
+
+CampoSelect.propTypes = {
+  label: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  onChange: PropTypes.func,
+  options: PropTypes.arrayOf(
+    PropTypes.shape({
+      value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+      label: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  fullWidth: PropTypes.bool,
+  required: PropTypes.bool,
+  error: PropTypes.bool,
+  helperText: PropTypes.string,
+  disabled: PropTypes.bool,
+  size: PropTypes.oneOf(['small', 'medium']),
+};
+
+export default CampoSelect;

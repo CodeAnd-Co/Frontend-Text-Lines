c287114bc26501d0d812d4e73c5c7b565499572a
Angeltrek <angeltrek007@gmail.com>
2025-04-16 12:30:57 -0600

    Renombrar direccion de componente

--- src/vistas/stories/CampoTexto.stories.js
+++ src/vistas/stories/CampoTexto.stories.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import CampoTexto from '../componentes/atomos/CampoTexto';
+import CampoTexto from '../Componentes/Atomos/CampoTexto';
 
 export default {
   title: 'Componentes/Átomos/CampoTexto',

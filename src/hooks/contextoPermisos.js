0f5ca339b9a6ed3e5bcd2013fb7457440d55d221
PAOLA MARIA garrido <you@example.com>
2025-04-24 19:12:03 -0600

    fix: Eliminar archivo que no se usa.

--- src/hooks/contextoPermisos.js
+++
@@ -1,8 +0,0 @@
-export const usePermisos = () => {
-    const { permisos } = useContext(AuthContext); 
-  
-    const tienePermiso = (permiso) => permisos?.includes(permiso);
-  
-    return { tienePermiso };
-  };
-  

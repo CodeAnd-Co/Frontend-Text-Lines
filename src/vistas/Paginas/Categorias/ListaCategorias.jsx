3c6b031c1f2491768fb3a44f02c9068f6669dacc
angieriosc <A01705651@tec.mx>
2025-04-27 18:58:58 -0600

    Correcciones pull develop

--- src/vistas/Paginas/Categorias/ListaCategorias.jsx
+++ src/vistas/Paginas/Categorias/ListaCategorias.jsx
@@ -1,11 +1,12 @@
-<<<<<<< HEAD
 import React, { useState } from 'react';
 import Tabla from '../../Componentes/Organismos/Tabla';
-import { useConsultarCategorias } from '../../../hooks/Categorias/useConsultarCategorias';
 import Alerta from '../../Componentes/moleculas/Alerta';
 import ContenedorLista from '../../Componentes/Organismos/ContenedorLista';
 import ModalEliminarCategoria from '../../componentes/Organismos/ModalEliminarCategoria';
+import { useConsultarCategorias } from '../../../hooks/Categorias/useConsultarCategorias';
 import { Box } from '@mui/material';
+import { useTheme } from '@mui/material';
+import { tokens } from '../../../theme';
 
 const ListaCategorias = () => {
   const { categorias, cargando, error, recargar } = useConsultarCategorias();
@@ -13,36 +14,19 @@ const ListaCategorias = () => {
   const [openModalEliminar, setOpenModalEliminar] = useState(false);
   const [alerta, setAlerta] = useState(null);
   const [idsCategoria, setIdsCategoria] = useState([]);
-=======
-import React from 'react';
-import { useConsultarCategorias } from '../../../hooks/Categorias/useConsultarCategorias';
-import CustomDataGrid from '../../Componentes/Organismos/Tabla';
-import Alerta from '../../componentes/moleculas/Alerta';
-import ContenedorLista from '../../componentes/organismos/ContenedorLista';
-import { useTheme } from '@mui/material';
-import { tokens } from '../../../theme';
-
-/**
- * Página para consultar y mostrar la lista de categorías en una tabla.
- *
- * Muestra los resultados en un `CustomDataGrid`, incluyendo
- * nombre, descripción y número de productos de cada categoría.
- *
- * @see [RF[47] Consulta lista de categorías](https://codeandco-wiki.netlify.app/docs/proyectos/textiles/documentacion/requisitos/RF47)
- */
-
-const ListaCategorias = () => {
-  // Hook que obtiene las categorías desde el repositorio
-  const { categorias, cargando, error } = useConsultarCategorias();
   const theme = useTheme();
-  const colors = tokens(theme.palette.mode);
->>>>>>> 54f424e036e6765e2e98be9eb1b0f12e08b94053
+  const colores = tokens(theme.palette.mode);
 
   // Columnas para el DataGrid
   const columns = [
     { field: 'nombreCategoria', headerName: 'Nombre', flex: 1 },
     { field: 'descripcion', headerName: 'Descripción', flex: 2 },
-    { field: 'cantidadProductos', headerName: 'Número de productos asociados', type: 'number', flex: 1 },
+    {
+      field: 'cantidadProductos',
+      headerName: 'Número de productos asociados',
+      type: 'number',
+      flex: 1,
+    },
   ];
 
   const rows = categorias.map((cat) => ({
@@ -54,25 +38,21 @@ const ListaCategorias = () => {
   }));
 
   const botones = [
-<<<<<<< HEAD
     {
       label: 'Añadir',
       onClick: () => console.log('Añadir'),
       size: 'large',
+      color: 'primary',
+      backgroundColor: colores.altertex[1],
     },
     {
       variant: 'outlined',
-      label: 'Importar',
+      label: 'Editar',
       onClick: () => console.log('Importar'),
+      color: 'primary',
       size: 'large',
+      outlineColor: colores.altertex[1],
     },
-    {
-      variant: 'outlined',
-      label: 'Exportar',
-      onClick: () => console.log('Exportar'),
-      size: 'large',
-    },
-    { variant: 'outlined', label: 'Editar', onClick: () => console.log('Editar'), size: 'large' },
     {
       label: 'Eliminar',
       onClick: () => {
@@ -89,7 +69,9 @@ const ListaCategorias = () => {
           setOpenModalEliminar(true);
         }
       },
+      color: 'error',
       size: 'large',
+      backgroundColor: colores.altertex[1],
     },
   ];
 
@@ -132,24 +114,6 @@ const ListaCategorias = () => {
         />
       )}
     </>
-=======
-    { label: 'Añadir', variant: 'contained', color: 'primary', size: 'large', backgroundColor: colors.altertex[1], onClick: () => console.log('Añadir') },
-    { label: 'Editar', variant: 'outlined', color: 'primary', size: 'large', outlineColor: colors.altertex[1], onClick: () => console.log('Editar') },
-    { label: 'Eliminar', variant: 'contained', color: 'error', size: 'large', backgroundColor: colors.altertex[1], onClick: () => console.log('Eliminar') },
-  ];
-  
-  return (
-    <ContenedorLista
-      titulo="Categorías"
-      descripcion= "Gestiona las categorías de productos disponibles."
-      informacionBotones={botones}
-    >
-      <div style={{ height: 400, width: '100%' }}>
-        {error && <Alerta tipo="error" mensaje={error} icono cerrable centradoInferior />}
-        <CustomDataGrid columns={columns} rows={rows} loading={cargando} checkboxSelection />
-      </div>
-    </ContenedorLista>
->>>>>>> 54f424e036e6765e2e98be9eb1b0f12e08b94053
   );
 };
 

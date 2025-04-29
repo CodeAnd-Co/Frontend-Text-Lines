81404cb17821c7e81b13412c4b4f6d1021493ebf
Hiram <147564077+Hiram10tec@users.noreply.github.com>
2025-04-29 04:14:06 -0600

    Mejoras en la interfaz

--- src/vistas/Paginas/Productos/ListaProductos.jsx
+++ src/vistas/Paginas/Productos/ListaProductos.jsx
@@ -1,62 +1,100 @@
-// RF[27] Consulta Lista de Productos - [https://codeandco-wiki.netlify.app/docs/proyectos/textiles/documentacion/requisitos/RF27]
-// RF[30] - Elimina producto - https://codeandco-wiki.netlify.app/docs/proyectos/textiles/documentacion/requisitos/RF30
+//RF[27] Consulta Lista de Productos - [https://codeandco-wiki.netlify.app/docs/proyectos/textiles/documentacion/requisitos/RF27]
+//RF[30] Elimina Producto - [https://codeandco-wiki.netlify.app/docs/proyectos/textiles/documentacion/requisitos/RF30]
 import React, { useState } from 'react';
 import Tabla from '../../Componentes/Organismos/Tabla';
-import Alerta from '../../Componentes/moleculas/Alerta';
 import ContenedorLista from '../../Componentes/Organismos/ContenedorLista';
+import Alerta from '../../Componentes/moleculas/Alerta';
 import ModalEliminarProducto from '../../componentes/organismos/ModalEliminarProducto';
 import { useConsultarProductos } from '../../../hooks/Productos/useConsultarProductos';
 import { Box, useTheme } from '@mui/material';
 import { tokens } from '../../../theme';
 
-/**
- * Página para consultar y mostrar la lista de productos.
- */
 const ListaProductos = () => {
   const { productos, cargando, error, recargar } = useConsultarProductos();
+  const tema = useTheme();
+  const colores = tokens(tema.palette.mode);
+
   const [seleccionados, setSeleccionados] = useState(new Set());
   const [alerta, setAlerta] = useState(null);
   const [idsProducto, setIdsProducto] = useState([]);
   const [openModalEliminar, setOpenModalEliminar] = useState(false);
 
-  const theme = useTheme();
-  const colores = tokens(theme.palette.mode);
-
   const columnas = [
-    { field: 'nombreComun', headerName: 'Nombre', flex: 1 },
-    { field: 'precioVenta', headerName: 'Precio Venta', type: 'number', flex: 0.7 },
+    {
+      field: 'imagen',
+      headerName: 'Imagen',
+      flex: 0.5,
+      renderCell: (params) => (
+        <img
+          src={params.row.urlImagen}
+          alt='Producto'
+          style={{ width: 50, height: 50, objectFit: 'cover' }}
+        />
+      ),
+    },
+    {
+      field: 'nombreComun',
+      headerName: 'Nombre',
+      flex: 1,
+      cellClassName: 'name-column--cell',
+    },
+    {
+      field: 'precioVenta',
+      headerName: 'Precio Venta',
+      type: 'number',
+      flex: 0.7,
+      headerAlign: 'center',
+      align: 'center',
+    },
     {
       field: 'estado',
-      headerName: 'Disponibilidad',
+      headerName: 'Disponibilidad en stock',
       flex: 1,
-      valueGetter: ({ row }) => (row?.estado === 1 ? 'Disponible' : 'No disponible'),
+      cellClassName: 'estado-row--cell',
+      renderCell: ({ row: { estado } }) => {
+        return (
+          <Box
+            width='110px'
+            height='50%'
+            m='10px auto'
+            p='15px'
+            display='flex'
+            justifyContent='center'
+            alignItems='center'
+            color={estado === 1 ? colores.primario[4] : colores.texto[1]}
+            backgroundColor={estado === 1 ? colores.altertex[1] : colores.acciones[1]}
+            borderRadius='4px'
+          >
+            {estado === 1 ? 'Disponible' : 'No disponible'}
+          </Box>
+        );
+      },
     },
   ];
 
-  const filas = productos.map((producto) => ({
-    id: producto.idProducto,
-    nombreComun: producto.nombreComun,
-    precioVenta: producto.precioVenta,
-    estado: producto.estado,
+  const filas = productos.map((prod) => ({
+    id: prod.idProducto,
+    nombreComun: prod.nombreComun,
+    precioVenta: prod.precioVenta,
+    estado: prod.estado,
+    urlImagen: prod.urlImagen,
   }));
 
   const botones = [
+    { label: 'Añadir', onClick: () => console.log('Añadir'), size: 'large' },
     {
-      label: 'Añadir',
-      variant: 'contained',
-      color: 'primary',
+      variant: 'outlined',
+      label: 'Importar',
+      onClick: () => console.log('Importar'),
       size: 'large',
-      backgroundColor: colores.altertex[1],
-      onClick: () => console.log('Añadir producto'),
     },
     {
       variant: 'outlined',
-      label: 'Editar',
-      onClick: () => console.log('Editar producto'),
-      color: 'primary',
+      label: 'Exportar',
+      onClick: () => console.log('Exportar'),
       size: 'large',
-      outlineColor: colores.altertex[1],
     },
+    { variant: 'outlined', label: 'Editar', onClick: () => console.log('Editar'), size: 'large' },
     {
       label: 'Eliminar',
       onClick: () => {
@@ -73,26 +111,25 @@ const ListaProductos = () => {
           setOpenModalEliminar(true);
         }
       },
-      color: 'error',
       size: 'large',
-      backgroundColor: colores.altertex[1],
     },
   ];
 
   return (
     <>
       <ContenedorLista
-        titulo="Productos"
-        descripcion="Gestiona y organiza los productos registrados en el sistema."
+        titulo='Lista de Productos'
+        descripcion='Gestiona y organiza los productos registrados en el sistema.'
         informacionBotones={botones}
       >
-        <Box style={{ height: 400, width: '100%' }}>
-          {error && <Alerta tipo="error" mensaje={error} icono cerrable centradoInferior />}
+        <Box width={'100%'}>
+          {error && <Alerta tipo='error' mensaje={error} icono cerrable centradoInferior />}
           <Tabla
             columns={columnas}
             rows={filas}
             loading={cargando}
             checkboxSelection
+            rowHeight={80}
             onRowSelectionModelChange={(newSelection) => setSeleccionados(newSelection)}
           />
         </Box>

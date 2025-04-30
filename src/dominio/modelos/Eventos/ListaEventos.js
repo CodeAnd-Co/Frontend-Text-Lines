8f3b906d246695f6c41e10b2f00eb24d6d9b295c
Rodrigo Antonio Benítez De La Portilla <a01771433@tec.mx>
2025-04-30 12:43:06 -0600

    fix:eslint

--- src/dominio/modelos/Eventos/ListaEventos.js
+++ src/dominio/modelos/Eventos/ListaEventos.js
@@ -1,10 +1,10 @@
 import { Evento } from './Eventos';
 
 export class ListaEventos {
-  constructor({ mensaje, lista_eventos }) {
+  constructor({ mensaje, listaEventos }) {
     this.mensaje = mensaje;
-    this.eventos = Array.isArray(lista_eventos)
-      ? lista_eventos.map((evento) => new Evento(evento))
+    this.eventos = Array.isArray(listaEventos)
+      ? listaEventos.map((evento) => new Evento(evento))
       : [];
   }
 }

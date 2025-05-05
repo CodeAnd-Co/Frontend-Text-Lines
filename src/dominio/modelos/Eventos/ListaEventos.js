e90bb8c187e10f8c6adc1d4dd321ecadf9711df3
Ubuntu <ubuntu@ip-172-31-90-178.ec2.internal>
2025-05-05 20:30:53 +0000

    fix: eliminar archivo dominio con d minuscula

--- src/dominio/modelos/Eventos/ListaEventos.js
+++
@@ -1,9 +0,0 @@
-import { Evento } from './Eventos';
-
-export class ListaEventos {
-  constructor({ mensaje, listaEventos }) {
-    this.mensaje = mensaje;
-    const arr = listaEventos || [];
-    this.eventos = Array.isArray(arr) ? arr.map((evento) => new Evento(evento)) : [];
-  }
-}

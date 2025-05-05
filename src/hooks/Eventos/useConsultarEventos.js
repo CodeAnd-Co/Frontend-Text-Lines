3a5f27a74f4ea5dec9039f3cc24eaa8202e9303a
DiegoRAP2003 <diegoalfaropinto12@gmail.com>
2025-05-05 14:23:56 -0600

    arreglar el build

--- src/hooks/Eventos/useConsultarEventos.js
+++ src/hooks/Eventos/useConsultarEventos.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { RepositorioConsultarEventos } from '@SRC/Dominio/Repositorios/Eventos/RepositorioConsultarEventos';
+import { RepositorioConsultarEventos } from '@Repositorios/Eventos/RepositorioConsultarEventos';
 
 /**
  * Hook para consultar la lista de eventos.

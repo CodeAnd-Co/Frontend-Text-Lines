abb1b11ee74d7eed311a3db12751527f4eafa404
DiegoRAP2003 <diegoalfaropinto12@gmail.com>
2025-05-06 16:07:13 -0600

    fix: arreglar modales de crear

--- src/Vistas/Componentes/Organismos/ProductosModal.jsx
+++ src/Vistas/Componentes/Organismos/ProductosModal.jsx
@@ -1,17 +1,14 @@
-import Contenedor from '@Atomos/Contenedor';
 import Tabla from '@Organismos/Tabla';
 
 const ProductosModal = ({ elevacion, sx, columnas, filas, paginacion, checkBox, onRowClick }) => {
   return (
-    <Contenedor elevation={elevacion} sx={sx}>
-      <Tabla
-        columns={columnas}
-        rows={filas}
-        pageSize={paginacion}
-        checkboxSelection={checkBox}
-        onRowClick={onRowClick}
-      />
-    </Contenedor>
+    <Tabla
+      columns={columnas}
+      rows={filas}
+      pageSize={paginacion}
+      checkboxSelection={checkBox}
+      onRowClick={onRowClick}
+    />
   );
 };
 

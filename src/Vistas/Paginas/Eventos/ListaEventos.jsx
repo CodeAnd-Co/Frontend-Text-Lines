6d2183416ac75cd4b02303bf062c2a89cea1c00b
Rodrigo Antonio Benítez De La Portilla <a01771433@tec.mx>
2025-05-06 21:13:10 -0600

    feat: implementación de eliminar evento en ListaEvento.jsx

--- src/Vistas/Paginas/Eventos/ListaEventos.jsx
+++ src/Vistas/Paginas/Eventos/ListaEventos.jsx
@@ -1,16 +1,54 @@
 //RF37 - Consulta Lista de Eventos - https://codeandco-wiki.netlify.app/docs/proyectos/textiles/documentacion/requisitos/RF37
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, useTheme } from '@mui/material';
 import Tabla from '@Organismos/Tabla';
 import ContenedorLista from '@Organismos/ContenedorLista';
 import { useConsultarEventos } from '@Hooks/Eventos/useConsultarEventos';
 import { tokens } from '@SRC/theme';
+import { useAuth } from '@Hooks/AuthProvider';
+import { PERMISOS } from '@Utilidades/Constantes/permisos';
+import { useEliminarEvento } from '@Hooks/Eventos/useEliminarEvento';
+import PopUp from '@Moleculas/PopUp';
+import Alerta from '@Moleculas/Alerta';
+import Chip from '@Atomos/Chip';
 
 const ListaEventos = () => {
   const theme = useTheme();
   const colores = tokens(theme.palette.mode);
   const { eventos, cargando, error } = useConsultarEventos();
-  console.log('Eventos:', eventos); // Para depuración
+  const MENSAJES_POPUP_ELIMINAR = '¿Estás seguro de que deseas eliminar los eventos seleccionados?';
+
+  const [seleccionados, setSeleccionados] = useState([]);
+  const [alerta, setAlerta] = useState(null);
+  const { eliminar } = useEliminarEvento();
+  const [abrirEliminar, setAbrirPopUpEliminar] = useState(false);
+  const manejarCancelarEliminar = () => {
+    setAbrirPopUpEliminar(false);
+  };
+  const { usuario } = useAuth();
+  const manejarConfirmarEliminar = async () => {
+    try {
+      await eliminar(seleccionados);
+      setAlerta({
+        tipo: 'success',
+        mensaje: 'Eventos eliminados correctamente.',
+        icono: true,
+        cerrable: true,
+        centradoInferior: true,
+      });
+      setSeleccionados([]);
+    } catch {
+      setAlerta({
+        tipo: 'error',
+        mensaje: 'Ocurrió un error al eliminar los eventos.',
+        icono: true,
+        cerrable: true,
+        centradoInferior: true,
+      });
+    } finally {
+      setAbrirPopUpEliminar(false);
+    }
+  };
 
   const columnas = [
     { field: 'nombre', headerName: 'Nombre', flex: 1 },
@@ -47,7 +85,20 @@ const ListaEventos = () => {
     },
     {
       label: 'Eliminar',
-      onClick: () => console.log('Eliminar'),
+      onClick: () => {
+        if (seleccionados.length === 0) {
+          setAlerta({
+            tipo: 'error',
+            mensaje: 'Selecciona al menos un evento para eliminar.',
+            icono: true,
+            cerrable: true,
+            centradoInferior: true,
+          });
+        } else {
+          setAbrirPopUpEliminar(true);
+        }
+      },
+      disabled: !usuario?.permisos?.includes(PERMISOS.ELIMINAR_EVENTO),
       size: 'large',
       color: 'error',
       backgroundColor: colores.altertex[1],
@@ -61,8 +112,22 @@ const ListaEventos = () => {
       informacionBotones={botones}
     >
       <Box width={'100%'}>
+        {alerta && <Alerta {...alerta} onClose={() => setAlerta(null)} />}
         {error && <p style={{ color: 'red' }}>Error: {error}</p>}
-        <Tabla columns={columnas} rows={filas} loading={cargando} checkboxSelection />
+        <Tabla
+          columns={columnas}
+          rows={filas}
+          loading={cargando}
+          checkboxSelection
+          onRowSelectionModelChange={setSeleccionados}
+          selectionModel={seleccionados}
+        />
+        <PopUp
+          open={abrirEliminar}
+          onClose={manejarCancelarEliminar}
+          onConfirm={manejarConfirmarEliminar}
+          mensaje={MENSAJES_POPUP_ELIMINAR}
+        />
       </Box>
     </ContenedorLista>
   );

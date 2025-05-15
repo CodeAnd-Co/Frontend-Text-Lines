f34ec4c9b3cfd1f51be0240162ae51bb0c792644
Diego Alfaro Pinto <a01709971@tec.mx>
2025-05-15 17:14:36 -0600

    feat: arreglar errores

--- src/Utilidades/Constantes/rutasAPI.js
+++ src/Utilidades/Constantes/rutasAPI.js
@@ -41,7 +41,7 @@ export const RUTAS_API = {
     ELIMINAR_CLIENTE: `${BASE_CLIENTES}/eliminar`,
     CONSULTAR_CLIENTE: `${BASE_CLIENTES}/consultar-cliente`,
     ACTUALIZAR_CLIENTE: `${BASE_CLIENTES}/actualizar-cliente`,
-    CREAR_CLIENTE: `${BASE_CLIENTES}/crear`,
+    CREAR_CLIENTE: `${BASE_CLIENTES}/crear-cliente`,
   },
   EMPLEADOS: {
     BASE: BASE_EMPLEADOS,

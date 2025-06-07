2ed3c95c0524affe1648f058a2e4c9e13631ed0d
ArturoSanRod <a01275427@tec.mx>
2025-06-06 19:39:22 -0600

    Fix de consol logs

--- src/Dominio/Repositorios/Cuotas/repositorioObtenerOpcionesCuotas.js
+++ src/Dominio/Repositorios/Cuotas/repositorioObtenerOpcionesCuotas.js
@@ -6,7 +6,6 @@ const API_KEY = import.meta.env.VITE_API_KEY;
 export class RepositorioObtenerOpcionesCuotas {
   static async obtenerOpciones(idCliente) {
     try {
-      console.log('🔍 Solicitando opciones para cliente:', idCliente);
       
       const respuesta = await axios.get(
         `${RUTAS_API.CUOTAS.OBTENER_OPCIONES}?idCliente=${idCliente}`,
@@ -19,7 +18,7 @@ export class RepositorioObtenerOpcionesCuotas {
       console.log('🔍 Respuesta del backend - opciones:', respuesta.data);
       return respuesta.data;
     } catch (error) {
-      console.error('❌ Error en repositorio opciones:', error);
+      console.error('Error en repositorio opciones:', error);
       const mensaje = error.response?.data?.mensaje || 'Error al obtener opciones de productos.';
       throw new Error(mensaje);
     }
